3b4125e38ccf21a007bcc3d12b29bb29850ad954
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-04-03 22:03:02 +0900

    Init: 초기 설정

---
+++ src/main/java/com/example/chatClips/ChatClipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatClips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatClipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatClipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatClips/ChatClipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatClips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

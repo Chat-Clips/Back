b61cd6248b665aa7457baed7ad27c3438e00cc44
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-05-07 14:40:11 +0900

    feat: 소켓 시큐리티 설정 ë³구현

--- src/main/java/com/example/chatClips/config/SecurityConfig.java
+++ src/main/java/com/example/chatClips/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -48,7 +51,8 @@ public class SecurityConfig {
             .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
             .requestMatchers("/", "/login", "/loginProc", "/loginForm", "/joinForm", "/user/signup", "/user/login").permitAll() //모든 사용자에게 허용
             .requestMatchers("/admin").hasRole("ADMIN") //"ADMIN" 권한을 가진 사람만 허용
-            .requestMatchers("/user/**", "/feedback/**", "/chatroom/**", "/api/**").hasAnyRole("USER") //"ADMIN"과 "USER"만 허용
+            .requestMatchers("/user/**", "/feedback/**", "/chatroom/**", "/api/**","/ws/**").permitAll()
+            //.requestMatchers("/user/**", "/feedback/**", "/chatroom/**", "/api/**").hasAnyRole("USER") //"ADMIN"과 "USER"만 허용
             .anyRequest().authenticated()
         );
 
@@ -59,6 +63,20 @@ public class SecurityConfig {
             .permitAll()
         );
         http.csrf((auth) -> auth.disable());
+
+        http.cors((cors) -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowCredentials(true);
+                config.addAllowedOriginPattern("*"); // 모든 도메인 허용
+                config.addAllowedHeader("*");
+                config.addAllowedMethod("*");
+                return config;
+            };
+            cors.configurationSource(source);
+        });
+
+        //http.csrf((csrf) -> csrf.ignoringRequestMatchers("/ws/**"));
 
         http.sessionManagement((auth) -> auth
             .maximumSessions(1)

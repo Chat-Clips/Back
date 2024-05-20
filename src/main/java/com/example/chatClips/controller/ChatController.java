617057df012f31de24841a76ea9aab6b13592c54
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-05-21 01:43:54 +0900

    feat: 회원 방 나가기 구현

--- src/main/java/com/example/chatClips/controller/ChatController.java
+++ src/main/java/com/example/chatClips/controller/ChatController.java
@@ -47,6 +47,7 @@ public class ChatController {
 
         headerAccessor.getSessionAttributes().put("userId", userId);
         headerAccessor.getSessionAttributes().put("roomId", chat.getRoomId());
+        System.out.println((String) headerAccessor.getSessionAttributes().get("userId")+"\n"+ (String) headerAccessor.getSessionAttributes().get("roomId")+"\n");
         User user = userRepository.findByUserId(userId);
         chat.setMessage(user.getUsername() + "님이 입장하셨습니다.");
         template.convertAndSend("/sub/chatroom/" + chat.getRoomId(), chat);
@@ -66,32 +67,6 @@ public class ChatController {
         chatRepository.save(chatting);
         template.convertAndSend("/sub/chatroom/" + chat.getRoomId(), chat);
     }
-//    @PostMapping("/exitChatRoom")
-//    public void webSocketDisconnectListener(SessionDisconnectEvent event){
-//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-//
-//        String userId = (String) headerAccessor.getSessionAttributes().get("userId");
-//        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
-//
-//        chatRoomService.decreaseUser(roomId);
-//
-//        //채팅방 유저 리스트에서 UUID 유저 닉네임 조회 및 리스트에서 유저 삭제
-//        String userName = chatRoomService.getUserName(roomId, userId);
-//        chatRoomService.deleteUser(roomId,userId);
-//
-//        if(userName != null){
-//            log.info("User Disconnected : " + userName);
-//
-//            ChatDTO chat = ChatDTO.builder()
-//                .type(ChatDTO.MessageType.LEAVE)
-//                .sender(userId)
-//                .message(userName + "님이 퇴장하였습니다.")
-//                .build();
-//
-//            template.convertAndSend("/sub/chatroom/" + roomId,chat);
-//        }
-//    }
-//
     @EventListener
     public void webSocketDisconnectListener(SessionDisconnectEvent event){
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
@@ -100,26 +75,25 @@ public class ChatController {
         String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
 
         chatRoomService.decreaseUser(roomId);
+
         //채팅방 유저 리스트에서 UUID 유저 닉네임 조회 및 리스트에서 유저 삭제
-        ////
-        List<User> userList = userChatRoomRepository.findByChatRoom(chatRoomRepository.findByRoomId(roomId));
-        for(User user : userList){
-            chatRoomService.decreaseUser(roomId);
-            chatRoomService.deleteUser(roomId, user.getUserId());
-            log.info("User Disconnected : " + user.getUserId());
+        String userName = chatRoomService.getUserName(roomId, userId);
+        chatRoomService.deleteUser(roomId,userId);
 
-//            ChatDTO chat = ChatDTO.builder()
-//                .type(ChatDTO.MessageType.LEAVE)
-//                .sender(userId)
-//                .message(userName + "님이 퇴장하였습니다.")
-//                .build();
-//
-//            template.convertAndSend("/sub/chatroom/" + roomId,chat);
+        if(userName != null){
+            log.info("User Disconnected : " + userName);
+
+            ChatDTO chat = ChatDTO.builder()
+                .type(ChatDTO.MessageType.LEAVE)
+                .sender(userId)
+                .message(userName + "님이 퇴장하였습니다.")
+                .build();
+
+            template.convertAndSend("/sub/chatroom/" + roomId,chat);
         }
-        ////
-
-
     }
+
+
 
 //    // 채팅에 참여한 유저 리스트 반환
 //    @GetMapping("/userlist")
